d15ff061f49d021f193618085095e10a5cae2cfe
silyas-uncc <silyas@charlotte.edu>
2026-03-27 20:02:11 -0400

    add: implemented the resource controller

---
+++ api/controllers/resources.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import models, schemas
+
+def create(db: Session, resource):
+    db_resource = models.Resource(
+        item=resource.item,
+        amount=resource.amount
+    )
+    db.add(db_resource)
+    db.commit()
+    db.refresh(db_resource)
+    return db_resource
+
+def read_all(db: Session):
+    return db.query(models.Resource).all()
+
+def read_one(db: Session, sandwich_id):
+    return db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id).first()
+
+def update(db: Session, resource_id, resource):
+    db_resource = db.query(models.Resource).filter(models.Resource.id == resource_id)
+    update_data = resource.model_dump(exclude_unset=True)
+    db_resource.update(update_data, synchronize_session=False)
+    db.commit()
+    return db_resource.first()
+
+def delete(db: Session, sandwich_id):
+    db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
+    db_sandwich.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
