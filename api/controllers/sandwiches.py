fb78dec5f0a7c2344ace0bd162b973f36e763def
silyas-uncc <silyas@charlotte.edu>
2026-03-27 20:44:16 -0400

    Fix: updated any typos

--- api/controllers/sandwiches.py
+++ api/controllers/sandwiches.py
@@ -15,7 +15,7 @@ def create(db: Session, sandwich):
 def read_all(db: Session):
     return db.query(models.Sandwich).all()
 
-def read_one(db: Session, sandwich_id, sandwich):
+def read_one(db: Session, sandwich_id):
     db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
     update_data = sandwich.model_dump(exclude_unset=True)
     db_sandwich.update(update_data, synchronize_session=False)
